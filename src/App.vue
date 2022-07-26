6dab2ef8c1bcd84e7731da052de0178214770cb8
Thibaud Auzou <thibaud.auzou@gmail.com>
2022-07-27 00:55:42 +0200

    WIP

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Button btnPrimary>Click me</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from 'drakarrui';
+import '@/assets/tailwind.scss';
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn" :class="classBinding">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import '@/assets/tailwind.scss';
+
+const props = defineProps<{ btnPrimary?: boolean }>();
+
+const classBinding = computed(() => ({
+  'btn-primary': !!props.btnPrimary,
+}));
+</script>
